588c4b135ab795715a297d6b2931f38867573140
Sam Ceja <sammmceja@gmail.com>
2023-04-19 21:50:45 -0700

    loading font + loading data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import _ from "lodash";
 import myName from "./myName";
 import "./style.css";
 import Dragon from "./dragon-logo.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
@@ -14,6 +16,9 @@ function component() {
 
   const myDragon = new Image();
   myDragon.src = Dragon;
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,22 @@ module.exports = {
 
         type: "asset/resource",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+
+        use: ["csv-loader"],
+      },
+
+      {
+        test: /\.xml$/i,
+
+        use: ["xml-loader"],
+      },
     ],
   },
 };
